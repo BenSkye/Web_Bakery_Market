896d7027c4eb6d966dc298bcfa5f4ff7b396a7f5
laptop-m8du3f9f <quangtran214203@gmail.com>
2024-08-16 11:50:32 +0700

    3d model

---
+++ web_bakery_marketplace_FE/src/pages/3D/3DHome.tsx
@@ -0,0 +1,6 @@
+import CakeModel from "./CakeModel";
+
+const CakeDesigner: React.FC = () => {
+  return <CakeModel />;
+};
+export default CakeDesigner;
--- web_bakery_marketplace_FE/src/routes/routers.tsx
+++ web_bakery_marketplace_FE/src/routes/routers.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import HomePage from '../pages/home/Home';
-import AboutPage from '../pages/about/About';
-import StoresPage from '../pages/stores/Stores';
-import WorkshopPage from '../pages/workshop/Workshop';
-import LoginPage from '../pages/login/Login';
-import MainLayout from '../layouts/DefaultLayout';
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "../pages/home/Home";
+import AboutPage from "../pages/about/About";
+import StoresPage from "../pages/stores/Stores";
+import WorkshopPage from "../pages/workshop/Workshop";
+import LoginPage from "../pages/login/Login";
+import MainLayout from "../layouts/DefaultLayout";
+import CakeDesigner from "../pages/3D/3DHome";
 
 const AppRoutes: React.FC = () => {
-    return (
-        <Routes>
-
-            <Route path="/login" element={<LoginPage />} />
-            <Route element={<MainLayout />}>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/about" element={<AboutPage />} />
-                <Route path="/stores" element={<StoresPage />} />
-                <Route path="/workshop" element={<WorkshopPage />} />
-            </Route>
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/stores" element={<StoresPage />} />
+        <Route path="/workshop" element={<WorkshopPage />} />
+        <Route path="/CakeDesigner" element={<CakeDesigner />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default AppRoutes;
