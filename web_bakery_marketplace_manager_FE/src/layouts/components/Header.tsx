f50db35c83ccedfd7467d5c01c7838decb1306b6
Duong Minh Nhat <nhatdmse173465@fpt.edu.vn>
2024-09-30 02:13:17 +0700

    update

--- web_bakery_marketplace_manager_FE/src/layouts/components/Header.tsx
+++ web_bakery_marketplace_manager_FE/src/layouts/components/Header.tsx
@@ -1,61 +1,55 @@
-import React, { useState } from 'react';
-import { Layout, Menu, Row, Col } from 'antd';
-import {
-    UserOutlined,
-    BarChartOutlined,
-    HomeOutlined,
-    ShoppingCartOutlined,
-    SettingOutlined,
-} from '@ant-design/icons';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Layout, Avatar, Button, Dropdown, Menu } from 'antd';
+import { UserOutlined, BellOutlined, DownOutlined } from '@ant-design/icons';
+import logo from '../../assets/logo.png'
+import imgFun from '../../assets/asset-13-16837926873.png'
+import { useAuth } from '../../stores/authContex';
 
 const { Header } = Layout;
 
-const HeaderComponent: React.FC = () => {
-    const [openKeys, setOpenKeys] = useState<string[]>([]);
+const UserHeader: React.FC = () => {
+    const { user, logout } = useAuth();
 
-    const handleOpenChange = (keys: string[]) => {
-        setOpenKeys(keys);
+    const handleLanguageChange = (lang: string) => {
+        // Xử lý thay đổi ngôn ngữ
+        console.log(`Đổi ngôn ngữ sang ${lang}`);
     };
 
+
+
+    const menu = (
+        <Menu>
+            <Menu.Item onClick={() => handleLanguageChange('Tiếng Việt')}>
+                Tiếng Việt
+            </Menu.Item>
+            <Menu.Item onClick={() => handleLanguageChange('English')}>
+                English
+            </Menu.Item>
+            <Menu.Divider />
+            <Menu.Item onClick={logout}>
+                Đăng xuất
+            </Menu.Item>
+        </Menu>
+    );
+
     return (
-        <Header style={{ background: '#f0f2f5', padding: '0 16px', boxShadow: '0 2px 8px rgba(0, 0, 0, 0.1)' }}>
-            <Row justify="space-between" align="middle">
-                <Col>
-                    <h2 style={{ margin: 0, fontWeight: 'bold', color: '#ff7e67' }}>Admin Dashboard</h2>
-                </Col>
-            </Row>
-            <Menu
-                mode="inline"
-                style={{ border: 'none', marginTop: '16px' }}
-                openKeys={openKeys}
-                onOpenChange={handleOpenChange}
-            >
-                <Menu.Item key="dashboard" icon={<UserOutlined />}>
-                    <Link to='/admin/dashboard'>Tài khoản</Link>
-                </Menu.Item>
-                <Menu.SubMenu key="statistics" title="Thống kê" icon={<BarChartOutlined />}>
-                    <Menu.Item key="stats-overview" icon={<BarChartOutlined />}>
-                        <Link to='/statistics'>Tổng quan</Link>
-                    </Menu.Item>
-                    <Menu.Item key="stats-reports" icon={<BarChartOutlined />}>
-                        <Link to='/statistics'>Báo cáo</Link>
-                    </Menu.Item>
-                </Menu.SubMenu>
-                <Menu.Item key="bakeries" icon={<HomeOutlined />}>
-                    <Link to='/'>Các tiệm</Link>
-                </Menu.Item>
-                <Menu.Item key="orders" icon={<ShoppingCartOutlined />}>
-                    <Link to='manage'>Đơn hàng</Link>
-                </Menu.Item>
-
-                <Menu.Item key="settings-profile" icon={<SettingOutlined />}>
-                    <Link to='/admin/settings/profile'>Cài đặt hồ sơ</Link>
-                </Menu.Item>
-
-            </Menu>
+        <Header style={{ background: 'white', padding: '0 24px', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+                <img src={logo} alt="Logo" style={{ height: '80px', marginRight: '10px' }} />
+                <span className="curvy-text">Merci<img src={imgFun} style={{ height: '40px', marginRight: '10px' }}></img> </span>
+            </div>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+                <Button shape="circle" icon={<BellOutlined />} style={{ marginRight: '16px' }} />
+                <Dropdown overlay={menu} trigger={['click']}>
+                    <div style={{ display: 'flex', alignItems: 'center', cursor: 'pointer' }}>
+                        <UserOutlined sizes='large' />
+                        {user ? <span style={{ marginLeft: '10px' }}>Xin chào, {user.name} </span> : <UserOutlined />}
+                        <DownOutlined style={{ marginLeft: '5px' }} />
+                    </div>
+                </Dropdown>
+            </div>
         </Header>
     );
 };
 
-export default HeaderComponent;
+export default UserHeader;
---
+++ web_bakery_marketplace_manager_FE/src/layouts/components/Navigation.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import { Layout, Menu, Row, Col } from 'antd';
+import {
+    UserOutlined,
+    BarChartOutlined,
+    HomeOutlined,
+    ShoppingCartOutlined,
+    SettingOutlined,
+} from '@ant-design/icons';
+import { Link } from 'react-router-dom';
+
+const { Header } = Layout;
+
+const NavigationComponent: React.FC = () => {
+    const [openKeys, setOpenKeys] = useState<string[]>([]);
+
+    const handleOpenChange = (keys: string[]) => {
+        setOpenKeys(keys);
+    };
+
+    return (
+        <Header style={{ background: '#f0f2f5', padding: '0 16px', boxShadow: '0 2px 8px rgba(0, 0, 0, 0.1)' }}>
+            <Row justify="space-between" align="middle">
+                <Col>
+                    <h2 style={{ margin: 0, fontWeight: 'bold', color: '#ff7e67' }}>Admin Dashboard</h2>
+                </Col>
+            </Row>
+            <Menu
+                mode="inline"
+                style={{ border: 'none', marginTop: '16px' }}
+                openKeys={openKeys}
+                onOpenChange={handleOpenChange}
+            >
+                {/* <Menu.Item key="dashboard" icon={<UserOutlined />}>
+                    <Link to='/admin/dashboard'>Tài khoản</Link>
+                </Menu.Item> */}
+                <Menu.SubMenu key="statistics" title="Thống kê" icon={<BarChartOutlined />}>
+                    <Menu.Item key="stats-overview" icon={<BarChartOutlined />}>
+                        <Link to='/statistics'>Tổng quan</Link>
+                    </Menu.Item>
+                    <Menu.Item key="stats-reports" icon={<BarChartOutlined />}>
+                        <Link to='/statistics'>Báo cáo</Link>
+                    </Menu.Item>
+                </Menu.SubMenu>
+                <Menu.Item key="bakeries" icon={<HomeOutlined />}>
+                    <Link to='/home'>Các tiệm</Link>
+                </Menu.Item>
+                <Menu.Item key="orders" icon={<ShoppingCartOutlined />}>
+                    <Link to='manage'>Đơn hàng</Link>
+                </Menu.Item>
+
+                <Menu.Item key="settings-profile" icon={<SettingOutlined />}>
+                    <Link to='/admin/settings/profile'>Cài đặt hồ sơ</Link>
+                </Menu.Item>
+
+            </Menu>
+        </Header>
+    );
+};
+
+export default NavigationComponent;
