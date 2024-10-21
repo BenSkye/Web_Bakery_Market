c4c1b6ce52b2622e99ec08b8c4f849432e252fe9
Duong Minh Nhat <nhatdmse173465@fpt.edu.vn>
2024-10-21 10:40:43 +0700

    update

--- web_bakery_marketplace_FE/.eslintrc.cjs
+++ web_bakery_marketplace_FE/.eslintrc.cjs
@@ -8,11 +8,18 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', '@typescript-eslint'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    // Bỏ qua cảnh báo về biến không sử dụng
+    '@typescript-eslint/no-unused-vars': [
+      'warn', // Chỉ cảnh báo thay vì lỗi
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }, // Bỏ qua các biến bắt đầu bằng dấu gạch dưới
+    ],
+    // Bỏ qua cảnh báo về tham số không sử dụng
+    '@typescript-eslint/no-unused-parameters': 'off', // Tắt hoàn toàn kiểm tra tham số không sử dụng
   },
-}
+};
